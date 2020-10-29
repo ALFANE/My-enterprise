05b2fb335d2c9aef9adab2fc61fa29f727126129
Your Name <you@example.com>
2020-10-29 22:39:42 +0200

    commit

---
+++ 1_Software_product_requirements/1.1_Determining_consumer_needs/1.1.2_Material_needs_details.mm
@@ -0,0 +1,124 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#33ccff" CREATED="1600717999918" ID="ID_1204507912" MODIFIED="1600719695409" TEXT="&#x420;&#x43e;&#x431;&#x43e;&#x442;&#x430; &#x43f;&#x456;&#x434;&#x43f;&#x440;&#x438;&#x454;&#x43c;&#x442;&#x441;&#x432;&#x430;">
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1600718562801" ID="ID_374123214" MODIFIED="1600719652363" POSITION="right" TEXT="&#x412;&#x456;&#x434;&#x434;&#x456;&#x43b;&#x438;">
+<edge COLOR="#00ff00" WIDTH="2"/>
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1600718608527" ID="ID_59890472" MODIFIED="1600719498785" TEXT="&#x412;&#x456;&#x434;&#x434;&#x456;&#x43b; &#x43a;&#x430;&#x434;&#x440;&#x456;&#x432;">
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1600718947507" ID="ID_1431514101" MODIFIED="1600719498785" TEXT="&#x41a;&#x430;&#x434;&#x440;&#x43e;&#x432;&#x438;&#x43a;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600718952592" ID="ID_1592332780" MODIFIED="1600719498785" TEXT="&#x41d;&#x430;&#x447;&#x430;&#x43b;&#x44c;&#x43d;&#x438;&#x43a; &#x432;&#x456;&#x434;&#x434;&#x456;&#x43b;&#x443; &#x43a;&#x430;&#x434;&#x440;&#x456;&#x432;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1600718618345" ID="ID_1617716507" MODIFIED="1600719498785" TEXT="&#x411;&#x443;&#x445;&#x433;&#x430;&#x43b;&#x442;&#x435;&#x440;&#x456;&#x44f; ">
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1600718966243" ID="ID_639570965" MODIFIED="1600719498785" TEXT="&#x411;&#x443;&#x445;&#x433;&#x430;&#x43b;&#x442;&#x435;&#x440;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600718972388" ID="ID_1270112276" MODIFIED="1600719498785" TEXT="&#x413;&#x43e;&#x43b;&#x43e;&#x432;&#x43d;&#x438;&#x439; &#x431;&#x443;&#x445;&#x433;&#x430;&#x43b;&#x442;&#x435;&#x440;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1600718627153" ID="ID_607457862" MODIFIED="1600719498785" TEXT="&#x421;&#x43a;&#x43b;&#x430;&#x434;">
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1600718987733" ID="ID_1339738102" MODIFIED="1600719498785" TEXT="&#x41a;&#x43e;&#x43c;&#x456;&#x440;&#x43d;&#x438;&#x43a;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600719021991" ID="ID_1157541468" MODIFIED="1600719498785" TEXT="&#x41d;&#x430;&#x447;&#x430;&#x43b;&#x44c;&#x43d;&#x438;&#x43a; &#x441;&#x43a;&#x43b;&#x430;&#x434;&#x443;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1600718632851" ID="ID_280327791" MODIFIED="1600719498785" TEXT="&#x412;&#x456;&#x434;&#x434;&#x456;&#x43b; &#x43f;&#x43e; &#x440;&#x43e;&#x431;&#x43e;&#x442;&#x456; &#x437; &#x43a;&#x43b;&#x456;&#x454;&#x43d;&#x442;&#x430;&#x43c;&#x438;">
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1600719071089" ID="ID_1874904403" MODIFIED="1600719498785" TEXT="&#x41c;&#x435;&#x43d;&#x435;&#x434;&#x436;&#x435;&#x440;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600719076833" ID="ID_966589605" MODIFIED="1600719498785" TEXT="&#x41d;&#x430;&#x447;&#x430;&#x43b;&#x44c;&#x43d;&#x438;&#x43a; &#x432;&#x456;&#x434;&#x434;&#x456;&#x43b;&#x443;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1600718742773" ID="ID_1695203766" MODIFIED="1600719498785" TEXT="&#x412;&#x456;&#x434;&#x434;&#x456;&#x43b; &#x43e;&#x431;&#x441;&#x43b;&#x443;&#x433;&#x43e;&#x432;&#x443;&#x432;&#x430;&#x43d;&#x43d;&#x44f;">
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1600719113544" ID="ID_1757561760" MODIFIED="1600719498785" TEXT="&#x406;&#x43d;&#x436;&#x435;&#x43d;&#x435;&#x440;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600719121216" ID="ID_1460653826" MODIFIED="1600719498785" TEXT="&#x41f;&#x440;&#x43e;&#x433;&#x440;&#x430;&#x43c;&#x456;&#x441;&#x442;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600719129741" ID="ID_1881413140" MODIFIED="1600719498784" TEXT="&#x41d;&#x430;&#x447;&#x430;&#x43b;&#x44c;&#x43d;&#x438;&#x43a; &#x432;&#x456;&#x434;&#x434;&#x456;&#x43b;&#x443;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600719138111" ID="ID_638295021" MODIFIED="1600719498784" TEXT="&#x414;&#x438;&#x441;&#x43f;&#x435;&#x442;&#x447;&#x435;&#x440;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600719159790" ID="ID_235115136" MODIFIED="1600719498784" TEXT="&#x421;&#x442;&#x430;&#x440;&#x448;&#x438;&#x439; &#x431;&#x440;&#x438;&#x433;&#x430;&#x434;&#x438;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600719230156" ID="ID_188099218" MODIFIED="1600719498784" TEXT="&#x41a;&#x435;&#x440;&#x456;&#x432;&#x43d;&#x438;&#x43a; &#x43f;&#x440;&#x43e;&#x435;&#x43a;&#x442;&#x443;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1600718754015" ID="ID_505215060" MODIFIED="1600719498784" TEXT="&#x414;&#x438;&#x440;&#x435;&#x43a;&#x442;&#x43e;&#x440; ">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1600719505920" ID="ID_474523980" MODIFIED="1600720318852" POSITION="left" TEXT="&#x41a;&#x43b;&#x456;&#x454;&#x43d;&#x442;&#x438;">
+<edge COLOR="#ff0033" WIDTH="2"/>
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1600719527402" ID="ID_829497335" MODIFIED="1600720318852" TEXT="&#x42e;&#x440;&#x438;&#x434;&#x438;&#x447;&#x43d;&#x456; &#x43e;&#x441;&#x43e;&#x431;&#x438;">
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1600719575717" ID="ID_147468462" MODIFIED="1600720318852" TEXT="&#x414;&#x435;&#x440;&#x436;&#x430;&#x432;&#x43d;&#x456;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600719584546" ID="ID_825261606" MODIFIED="1600720318851" TEXT="&#x427;&#x430;&#x441;&#x43d;&#x456;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1600719567606" ID="ID_1659568476" MODIFIED="1600720318851" TEXT="&#x424;&#x456;&#x437;&#x438;&#x447;&#x43d;&#x456; &#x43e;&#x441;&#x43e;&#x431;&#x438;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1600719740010" ID="ID_606136618" MODIFIED="1600720318851" POSITION="right" TEXT="&#x41f;&#x43e;&#x441;&#x43b;&#x443;&#x433;&#x438;">
+<edge COLOR="#990099" WIDTH="2"/>
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1600719745157" ID="ID_29807176" MODIFIED="1600720318851" TEXT="&#x420;&#x43e;&#x437;&#x440;&#x43e;&#x431;&#x43a;&#x430; &#x406;&#x421;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600719763902" ID="ID_1686780491" MODIFIED="1600720318850" TEXT="&#x41d;&#x430;&#x43b;&#x430;&#x448;&#x442;&#x443;&#x432;&#x430;&#x43d;&#x43d;&#x44f; &#x43a;&#x43e;&#x43c;&#x43f;&apos;&#x44e;&#x442;&#x435;&#x440;&#x43d;&#x43e;&#x457; &#x43c;&#x435;&#x440;&#x435;&#x436;&#x456;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600719793529" ID="ID_510130854" MODIFIED="1600720318850" TEXT="&#x41e;&#x431;&#x441;&#x43b;&#x443;&#x433;&#x43e;&#x432;&#x443;&#x432;&#x430;&#x43d;&#x43d;&#x44f; &#x43c;&#x435;&#x440;&#x435;&#x436;&#x456;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600719808929" ID="ID_901997820" MODIFIED="1600720318849" TEXT="&#x412;&#x43d;&#x435;&#x441;&#x442;&#x438; &#x437;&#x43c;&#x456;&#x43d;&#x438; &#x434;&#x43e; &#x456;&#x441;&#x43d;&#x443;&#x44e;&#x447;&#x43e;&#x433;&#x43e; &#x41f;&#x41f;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600719888892" ID="ID_341112377" MODIFIED="1600720318848" TEXT="&#x420;&#x43e;&#x437;&#x440;&#x43e;&#x431;&#x43a;&#x430; &#x41f;&#x417;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600719947547" ID="ID_1811262488" MODIFIED="1600720318848" TEXT="&#x420;&#x435;&#x43c;&#x43e;&#x43d;&#x442; &#x43e;&#x440;&#x433;&#x442;&#x435;&#x445;&#x43d;&#x456;&#x43a;&#x438;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1600720226232" ID="ID_76237572" MODIFIED="1600720318848" POSITION="left" TEXT="&#x41e;&#x431;&#x43b;&#x430;&#x434;&#x43d;&#x430;&#x43d;&#x43d;&#x44f;">
+<edge COLOR="#ffff00" WIDTH="2"/>
+<font NAME="Times New Roman" SIZE="16"/>
+<node CREATED="1600720233870" ID="ID_1436074262" MODIFIED="1600720318848" TEXT="&#x41a;&#x43e;&#x43c;&#x43f;&#x44c;&#x442;&#x435;&#x440;&#x438;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600720246756" ID="ID_1678136978" MODIFIED="1600720318846" TEXT="&#x41c;&#x424;&#x423;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600720251224" ID="ID_668952950" MODIFIED="1600720318846" TEXT="&#x41c;&#x435;&#x440;&#x435;&#x436;&#x43e;&#x432;&#x435; &#x43e;&#x431;&#x43b;&#x430;&#x434;&#x43d;&#x430;&#x43d;&#x43d;&#x44f;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+<node CREATED="1600720269098" ID="ID_1834520228" MODIFIED="1600720318845" TEXT="&#x41f;&#x440;&#x43e;&#x435;&#x43a;&#x442;&#x43e;&#x440;&#x438;">
+<font NAME="Times New Roman" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
